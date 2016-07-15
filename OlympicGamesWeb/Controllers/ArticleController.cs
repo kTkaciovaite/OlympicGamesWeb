6ad9b99e2264edbf7c2fecc8dbc9cdae83b82537
kTkaciovaite <karolina.tkaciovaite@gmail.com>
2016-07-15 12:13:55 +0300

    First

---
+++ OlympicGamesWeb/Controllers/ArticleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OlympicGames.DataAccess;
+
+namespace OlympicGamesWeb.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            var test = new ArticleDataService();
+            var test2 = test.Get(2);
+
+            return View(test2);
+        }
+    }
+}
